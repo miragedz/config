c7be0455ea8059a83c5434822589efcb783ad39c
miragedz <kingrachid2000@gmail.com>
2020-04-22 20:54:06 +0100

    Update key2.py

--- key2.py
+++ key2.py
@@ -1,41 +1,16 @@
-import sys, termios, tty, os, time
- 
-def getch():
-    fd = sys.stdin.fileno()
-    old_settings = termios.tcgetattr(fd)
-    try:
-        tty.setraw(sys.stdin.fileno())
-        ch = sys.stdin.read(1)
- 
-    finally:
-        termios.tcsetattr(fd, termios.TCSADRAIN, old_settings)
-    return ch
- 
-button_delay = 0.2
- 
+from getkey import getkey, keys
+from signal import pause
+
+
+x = True
 while True:
-    char = getch()
- 
-    if (char == "x"):
-        print("Stop!")
-        exit(0)
- 
-    if (char == "a"):
-        os.system("gpio mode 5 out")
-        time.sleep(button_delay)
- 
-    elif (char == "d"):
-        print("Right pressed")
-        time.sleep(button_delay)
- 
-    elif (char == "w"):
-        os.system("gpio mode 5 out")
-        time.sleep(button_delay)
- 
-    elif (char == "s"):
-        os.system("gpio mode 5 in")
-        time.sleep(button_delay)
- 
-    elif (char == "1"):
-        print("Number 1 pressed")
-        time.sleep(button_delay)
+    key = getkey()
+    
+    if key == keys.ENTER and x == True:
+        print("ENTER!")
+        x = not x 
+    else:
+        print("ok!")
+        x = not x
+
+pause()
