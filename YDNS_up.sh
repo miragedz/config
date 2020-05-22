a10d11fa99319c19b4573ef15e6e49ac286ceb9a
miragedz <kingrachid2000@gmail.com>
2020-05-22 12:12:00 +0100

    Add files via upload

---
+++ YDNS_up.sh
@@ -0,0 +1,3 @@
+current_ip=`curl --silent https://ydns.io/api/v1/ip`
+
+curl --basic -u "kingrachid2000@gmail.com:miragedz123" --silent https://ydns.io/api/v1/update/?host=miragedz.ydns.eu\&ip=${current_ip}
