161061b9bffb407c099dc7e4123d5bb0d11552a3
miragedz <kingrachid2000@gmail.com>
2023-09-20 10:00:42 +0100

    Update mirage.sh

--- mirage.sh
+++ mirage.sh
@@ -1,11 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 oh=~/.oh-my-zsh
 echo -e "#### Install dependency packages\n"
-sudo apt update && sudo apt -y upgrade && sudo apt -y install ffmpeg python zsh git aria2 openssh git  zsh cups nano mc  v4l-utils libpq5 python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev php php-mbstring hostapd dnsmasq samba samba-common-bin
+sudo apt update && sudo apt -y upgrade && sudo apt -y install ffmpeg micro  zsh git aria2 openssh git  zsh cups   v4l-utils libpq5 python-pip python-dev libssl-dev libcurl4-openssl-dev libjpeg-dev libz-dev php php-mbstring hostapd dnsmasq samba samba-common-bin
 pip install --upgrade pip
 sudo npm install -g playercast
 python -m pip install requests 
 yes | pip install youtube-dl httpserver
+sudo chmod 777 /var/lib/samba/usershares
 clear
 
 sh -c "$(wget https://raw.githubusercontent.com/kingrachid2000/config/master/zh_auto.sh -O -)"
