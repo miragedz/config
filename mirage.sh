2b038eeb0817e4dc61e08502d8155f059197c2b3
miragedz <kingrachid2000@gmail.com>
2026-07-24 22:01:31 +0100

    Install Zsh plugins and themes
    Added installation commands for various Zsh plugins and themes.

--- mirage.sh
+++ mirage.sh
@@ -7,10 +7,15 @@ clear
 
 echo -e "#### Install Oh my zh and plugin\n"
 sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+echo -e "#### Install auto suggestion \n"
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/plugins/zsh-autosuggestions
+echo -e "#### Install synthax-hightlighting \n"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
+echo -e "#### Install sh-completion \n"
 git clone https://github.com/zsh-users/zsh-completions ~/.oh-my-zsh/plugins/zsh-completions
+echo -e "#### Install zsh-hystory \n"
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/plugins/zsh-history-substring-search
+echo -e "#### Install powerlevel10 \n"
 git clone https://github.com/romkatv/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 clear
 
